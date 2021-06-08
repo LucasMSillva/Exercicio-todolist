a434504f05e68327fd8ebfcc6b39e28e271bc7b4
Lucas Morais Silva <71843447+LucasMSillva@users.noreply.github.com>
2021-06-08 00:04:35 -0300

    apagar
    apagar os arquivos do html do angular.

--- projeto/todolist/src/app/app.component.ts
+++ projeto/todolist/src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolist';
+
+tarefa : string ="";
+horario: string="";
+
+tarefas: Array<string> =[""]
+
+horarios: Array<string> =[""]
+
+adicionartarefa(){
+  this.tarefas.push(this.tarefa,this.horario);
+  this.tarefa ="", this.horario="";
 }
+
+// adicionarhora(){
+//   this.horarios.push(this.horario);
+//   this.horario ="";
+// }
+ }
